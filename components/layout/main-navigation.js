287a3343e6ce4efaa382efcebae7d6f9cdd1dbfc
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-02-18 19:32:11 +0100

    implement logo & main-navigation header components

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,17 +1,24 @@
 import Link from "next/link";
+import styles from "./main-navigation.module.css";
+import Logo from "./logo";
 
 export default function MainNavigation() {
-    return (
-        <header>
-            <Link bref='/'><Logo/></Link>
-            <nav>
-                <ul>
-                    <li>
-                        <Link href="/posts">Posts</Link>
-                        <Link href="/contact">Contact</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <Logo />
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/posts">Posts</Link>
+          </li>
+          <li>
+            {" "}
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
