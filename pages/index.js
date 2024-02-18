26c0340e230ea477856e8ad2968728baa7641790
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-02-19 00:33:59 +0100

    add mock posts data and image

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,40 @@
 import Hero from "../components/home-page/hero";
+import FeaturedPosts from "../components/home-page/featured-posts";
+
+const MOCK_POSTS = [
+    {
+        slug: "getting-started-nextjs",
+        title: "Next JS Post 1",
+        image: "getting-started-nextjs.png",
+        excerpt: "Next.js is a JavaScript framework for building user interfaces.",
+        date: "2021-01-01",
+    },   {
+        slug: "getting-started-nextjs",
+        title: "Next JS",
+        image: "getting-started-nextjs.png",
+        excerpt: "Next.js is a JavaScript framework for building user interfaces.",
+        date: "2021-01-01",
+    },   {
+        slug: "getting-started-nextjs",
+        title: "Next JS",
+        image: "getting-started-nextjs.png",
+        excerpt: "Next.js is a JavaScript framework for building user interfaces.",
+        date: "2021-01-01",
+    },   {
+        slug: "getting-started-nextjs",
+        title: "Next JS",
+        image: "getting-started-nextjs.png",
+        excerpt: "Next.js is a JavaScript framework for building user interfaces.",
+        date: "2021-01-01",
+    },
+
+]
 
 function HomePage() {
   return (
     <>
       <Hero />
-      <FeaturedPosts />
+      <FeaturedPosts posts={MOCK_POSTS} />
     </>
   );
 }
