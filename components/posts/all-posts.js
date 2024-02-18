0a75f9c5867004ac0c5691faccbd93b09005f1e5
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-02-19 00:48:43 +0100

    implement all-posts page with mock data

---
+++ components/posts/all-posts.js
@@ -0,0 +1,12 @@
+import styles from "./all-posts.module.css";
+import PostGrid from "./posts-grid";
+
+export default function AllPosts(props) {
+  const { posts } = props;
+  return (
+      <section className={styles.posts}>
+        <h1>All Posts</h1>
+        <PostGrid posts={posts} />
+      </section>
+  );
+}
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,7 +1,35 @@
+import AllPosts from "../../components/posts/all-posts";
+
+const MOCK_POSTS = [
+  {
+    slug: "getting-started-nextjs",
+    title: "Next JS Post 1",
+    image: "getting-started-nextjs.png",
+    excerpt: "Next.js is a JavaScript framework for building user interfaces.",
+    date: "2021-01-01",
+  },   {
+    slug: "getting-started-nextjs",
+    title: "Next JS",
+    image: "getting-started-nextjs.png",
+    excerpt: "Next.js is a JavaScript framework for building user interfaces.",
+    date: "2021-01-01",
+  },   {
+    slug: "getting-started-nextjs",
+    title: "Next JS",
+    image: "getting-started-nextjs.png",
+    excerpt: "Next.js is a JavaScript framework for building user interfaces.",
+    date: "2021-01-01",
+  },   {
+    slug: "getting-started-nextjs",
+    title: "Next JS",
+    image: "getting-started-nextjs.png",
+    excerpt: "Next.js is a JavaScript framework for building user interfaces.",
+    date: "2021-01-01",
+  },
+
+]
+
+
 export default function PostsPage() {
-    return (
-        <div>
-            <h1>Posts Page</h1>
-        </div>
-    );
+  return <AllPosts posts={MOCK_POSTS}/>
 }
