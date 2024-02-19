2b77eed1c279a238afcd5f59764c03207f8a9796
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-02-19 21:37:30 +0100

    [BE]: add contact API Route for contact form

---
+++ src/pages/api/contact.ts
@@ -0,0 +1,38 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+type ResponseData = {
+  message: string;
+};
+
+export default function handler(
+  request: NextApiRequest,
+  response: NextApiResponse<ResponseData>,
+) {
+  if (request.method === "POST") {
+    // receive data
+    const { email, name, message } = request.body;
+
+    // validate data
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !message ||
+      message.trim() === ""
+    ) {
+      response.status(422).json({
+        message: "Invalid input.",
+      });
+      return;
+    }
+
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+    //todo: store in db
+    response.status(201).json({ message: "Successfully stored message." });
+  }
+}
