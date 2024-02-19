8e3c67de9d804837049a8c51c93f610d46ee197e
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-02-19 01:04:12 +0100

    add css files & clean up

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,22 +1,24 @@
 import PostHeader from "./post-header";
-import styles from './post-content.module.css'
+import styles from "./post-content.module.css";
 
-const MOCK_POST =   {
-    slug: "getting-started-nextjs",
-    title: "Next JS Post 1",
-    image: "getting-started-nextjs.png",
-    excerpt: "Next.js is a JavaScript framework for building user interfaces.",
-    date: "2021-01-01",
-    content: '# This is a first post'
-}
+const MOCK_POST = {
+  slug: "getting-started-nextjs",
+  title: "Next JS Post 1",
+  image: "getting-started-nextjs.png",
+  excerpt: "Next.js is a JavaScript framework for building user interfaces.",
+  date: "2021-01-01",
+  content: "# This is a first post",
+};
 
 export default function PostContent(props) {
-    let { post } = props;
-    post = MOCK_POST
-    const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  let { post } = props;
+  post = MOCK_POST;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
-    return <article className={styles.content}>
-        <PostHeader title={post.title} image={imagePath}/>
-        {post.content}
+  return (
+    <article className={styles.content}>
+      <PostHeader title={post.title} image={imagePath} />
+      {post.content}
     </article>
+  );
 }
