efd988c41e28df372e6033b80562255324cf9bf5
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-02-19 22:46:39 +0100

    [BE]: add database.js utils

---
+++ src/lib/database.js
@@ -0,0 +1,15 @@
+import { MongoClient } from "mongodb";
+
+export async function connectToDatabase() {
+  return await MongoClient.connect(process.env.MONGODB_URI);
+}
+
+export async function insertDocument(client, collection, document) {
+  const db = client.db();
+  return await db.collection(collection).insertOne(document);
+}
+
+export async function getAllDocuments({ client, collection, sort, filter }) {
+  const db = client.db();
+  return await db.collection(collection).find(filter).sort(sort).toArray();
+}
