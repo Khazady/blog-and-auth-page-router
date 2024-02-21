d3d122916b7f848e51f5b149f82f500d6122f15f
Michael_Kalatsey <mkalatsey@remedypointsolutions.com>
2024-02-21 20:45:52 +0100

    [FE]: wrap app by nextAuth SessionProvider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,17 +2,18 @@ import "../styles/globals.css";
 import Layout from "../components/layout/layout";
 import { NotificationContextProvider } from "../store/notification-context";
 import MainHead from "../components/metadata/main-head";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <>
+    <SessionProvider session={session} refetchInterval={180 * 60}>
       <MainHead />
       <NotificationContextProvider>
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </NotificationContextProvider>
-    </>
+    </SessionProvider>
   );
 }
 
